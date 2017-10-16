35e25652c1721c11ba2763205071d886af43f6fd
Juliette Rapala <jrapala@gmail.com>
2017-10-16 14:13:24 -0500

    Fixing Firebase Code
    Recent searches populate from submit button click event. Release date is in corrected format. Title links direct you to movie website (eventually to Reel It search result).

--- Reel-It-development/assets/javascript/api3_combined.js
+++ Reel-It-development/assets/javascript/api3_combined.js
@@ -11,6 +11,31 @@ $(document).ready(function(){
       $('#movieGrid').append(placeholderSVG);
     }
 
+// Firebase | JavaScript Code
+// =====================================================================================
+
+  // Setup Variables 
+  // =====================================================================================
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZvaB7V67-3e53_b0-zoFZ8HcjgocLqZ0",
+    authDomain: "reel-it-2a1c2.firebaseapp.com",
+    databaseURL: "https://reel-it-2a1c2.firebaseio.com",
+    projectId: "reel-it-2a1c2",
+    storageBucket: "",
+    messagingSenderId: "105428438933"
+  };
+
+  firebase.initializeApp(config);        
+
+  var database = firebase.database();     
+
+  // New data variables
+  var favorited = false;
+  var yellowStar = '<i class="fa fa-star favoriteStar" aria-hidden="true" data-favorited="true"></i>';
+  var emptyStar = '<i class="fa fa-star-o favoriteStar" aria-hidden="true" data-favorited="false"></i>';
+
 
 // The Movie DB API | JavaScript Code
 // =====================================================================================
@@ -186,6 +211,15 @@ $("#submitButton").on("click", function(event) {
         // .append(captionDisplay);
         $(".castMain").append(castDiv);
       };
+
+    // Code for Firebase push
+      database.ref().push({
+        favorited: favorited,
+        movieName: title,
+        movieUrl: movieWebsite,
+        releaseDate: convertedReleased,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
 
   // Prepend Movie
     $("#movies-view").html(movieDiv);
@@ -549,87 +583,64 @@ $(function(){
   //   duration: 300,
   // });
 
-// Firebase | JavaScript Code
+// Firebase Main Processes 
 // =====================================================================================
 
-  // Setup Variables 
-  // =====================================================================================
+// Firebase watcher + initial loader 
+    database.ref().on("child_added", function(childSnapshot) {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAZvaB7V67-3e53_b0-zoFZ8HcjgocLqZ0",
-    authDomain: "reel-it-2a1c2.firebaseapp.com",
-    databaseURL: "https://reel-it-2a1c2.firebaseio.com",
-    projectId: "reel-it-2a1c2",
-    storageBucket: "",
-    messagingSenderId: "105428438933"
-  };
+    // Create row
+    var $movieRow = $('<tr>');
 
-  firebase.initializeApp(config);        
+    // Create cells
+    var $favoritedCell = $('<td>');
+    var $movieNameCell = $('<td>');
+    var $releaseDateCell = $('<td>');
 
-  var database = firebase.database();     
+    // Favorite cell
+    // If movie is favorited, display yellow start. If it is not, display blank star.
+    //$favoritedCell.attr('class', 'favoriteStar')
+    if (childSnapshot.val().favorited) {
+      $favoritedCell.html(yellowStar);
+    } else {
+      $favoritedCell.html(emptyStar);
+    };
+    $movieRow.append($favoritedCell);
 
-  // Data variables
-  var favorited = false;
-  var title = '';
-  var released = '';
-  var releaseDate = '';
-  var yellowStar = '<i class="fa fa-star favoriteStar" aria-hidden="true" data-favorited="true"></i>';
-  var emptyStar = '<i class="fa fa-star-o favoriteStar" aria-hidden="true" data-favorited="false"></i>';
-
-  // Main Processes 
-  // =====================================================================================
-
-  // Firebase watcher + initial loader 
-      database.ref().on("child_added", function(childSnapshot) {
-
-      // Create row
-      var $movieRow = $('<tr>');
-
-      // Create cells
-      var $favoritedCell = $('<td>');
-      var $movieNameCell = $('<td>');
-      var $releaseDateCell = $('<td>');
-
-      // Favorite cell
-      // If movie is favorited, display yellow start. If it is not, display blank star.
-      //$favoritedCell.attr('class', 'favoriteStar')
-      if (childSnapshot.val().favorited) {
-        $favoritedCell.html(yellowStar);
-      } else {
-        $favoritedCell.html(emptyStar);
-      };
-      $movieRow.append($favoritedCell);
-
-      // Movie name cell
+    // Movie name cell
+    if (childSnapshot.val().movieUrl.length > 0) {
       var $movieNameContents = $('<a>');
       $movieNameContents.attr('href', childSnapshot.val().movieUrl);
+      $movieNameContents.attr("target", "_blank")
       $movieNameContents.html(childSnapshot.val().movieName);
       $movieNameCell.html($movieNameContents);
-      $movieRow.append($movieNameCell);
+    } else {
+      var $movieName = $('<p>');
+      $movieNameCell.text(childSnapshot.val().movieName);
+    }
+    $movieRow.append($movieNameCell);
 
-      // Release date cell
-      $releaseDateCell.html(childSnapshot.val().releaseDate);
-      $movieRow.append($releaseDateCell);
+    // Release date cell
+    $releaseDateCell.html(childSnapshot.val().releaseDate);
+    $movieRow.append($releaseDateCell);
 
-      // Add row to table
-        $("#moviesGoHere").append($movieRow);
+    // Add row to table
+      $("#moviesGoHere").append($movieRow);
 
-      // Handle the errors
-      }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
+    // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
 
-    $("#moviesGoHere").delegate(".favoriteStar", "click", function() {
-        if ($(this).attr("data-favorited", "true")) {
-          $(this).empty();
-          $(this).parent().html(yellowStar);
-        } else {
-          $(this).empty();
-          $(this).parent().html(emptyStar); 
-        }
-      });
-
+  $("#moviesGoHere").delegate(".favoriteStar", "click", function() {
+      if ($(this).attr("data-favorited", "true")) {
+        $(this).empty();
+        $(this).parent().html(yellowStar);
+      } else {
+        $(this).empty();
+        $(this).parent().html(emptyStar); 
+      }
+    });  
 
 // End of $(document).ready function
 });
